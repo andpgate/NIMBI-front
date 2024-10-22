b7cebf44b04186086da0db59f0ad1bfedae99125
desgrac1aaa <decire2211835@correo.uis.edu.co>
2024-10-21 20:20:00 -0500

    Crear informe commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AccordionModule } from 'primeng/accordion';
+import { CrearInformeComponent } from './crear-informe/crear-informe.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [AppComponent, NotfoundComponent],
-    imports: [AppRoutingModule, AppLayoutModule,AccordionModule],
+    declarations: [AppComponent, NotfoundComponent, CrearInformeComponent],
+    imports: [AppRoutingModule, AppLayoutModule,AccordionModule, FormsModule],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
     ],
