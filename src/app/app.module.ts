c86a33cdd9a2d214442294f1751e26b2869ba1ad
desgrac1aaa <decire2211835@correo.uis.edu.co>
2024-10-22 11:09:25 -0500

    actu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AccordionModule } from 'primeng/accordion';
-import { CrearInformeComponent } from './crear-informe/crear-informe.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [AppComponent, NotfoundComponent, CrearInformeComponent],
+    declarations: [AppComponent, NotfoundComponent],
     imports: [AppRoutingModule, AppLayoutModule,AccordionModule, FormsModule],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
