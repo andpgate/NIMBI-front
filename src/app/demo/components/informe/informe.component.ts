c318249a00a88b990852559b9fa8e1b56681ac18
desgrac1aaa <decire2211835@correo.uis.edu.co>
2024-10-22 11:03:24 -0500

    informe commit 2

--- src/app/demo/components/informe/informe.component.ts
+++ src/app/demo/components/informe/informe.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-informe',
   standalone: true,
-  imports: [],
-  templateUrl: './informe.component.html'
+  imports: [FormsModule, CommonModule],
+  templateUrl: './informe.component.html',
+  styleUrls: ['./informe.component.css']
 })
 export class InformeComponent {
 
+  organizaciones: string[] = ['Empresa A', 'Empresa B', 'Empresa C'];
+  organizacion: string = '';
+  fechaInicial: string = '';
+  fechaFinal: string = '';
+
+  generarInforme() {
+    if (this.organizacion && this.fechaInicial && this.fechaFinal) {
+        console.log('Generando informe para:', this.organizacion, this.fechaInicial, this.fechaFinal);
+    } else {
+        console.error('Por favor complete todos los campos.');
+    }
+}
 }
