8514e4f30a109ab09073f85e8391cea8a5e43e01
Valentina Álvarez <valentinaav825@gmail.com>
2024-11-21 15:34:54 -0500

    actualizar logo

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -15,14 +15,14 @@ export class AppMenuComponent implements OnInit {
     ngOnInit() {
         this.model = [
             {
-                label: 'Venta computadoras',
+                label: 'Venta de celulares',
                 items: [
                     { label: 'Registrar venta', icon: 'pi pi-fw pi-shopping-cart', routerLink: ['/registrar'] },
                     { label: 'Consultar venta', icon: 'pi pi-fw pi-search', routerLink: ['/consultar'] },
                     { label: 'Crear informe de ventas', icon: 'pi pi-fw pi-file', routerLink: ['/informe'] }
                 ],
-                image: `<div class="center-image-container"> <img src="/assets/demo/images/galleria/logo.png" alt="image width="200" height="200""> </div>`
-            }
+                image: `<img src="/assets/demo/images/galleria/logo.png" alt="Logo" width="230" height="230">`            
+        }
         ];
     }
 }
